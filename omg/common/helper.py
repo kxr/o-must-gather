8eb4b8d26f62346eea567c3ccd30ce1bf9db3195
Khizer Naeem <khizernaeem@gmail.com>
2020-11-27 01:41:32 +0400

    Unified age and age2 functions into 1

--- omg/common/helper.py
+++ omg/common/helper.py
@@ -10,37 +10,22 @@ from omg.common.config import Config
 # We compare the time reported in the yaml definition,
 # with the timestamp of the yaml file (i.e, when it was generated).
 # This should give us age of the object at the time of must-gather
-# obj_time time is in standard format e.g: '2020-06-04T22:10:41Z'
-# file_ts is unix ts e.g: 1590912494.0 (returned by os.path.getmtime)
+# By default, ts1 is considered in iso-8601 e.g: '2020-06-04T22:10:41Z'
+# and ts2 is considered in unix/epoch format e.g: 1590912494.0 (returned by os.path.getmtime)
 
-def age(obj_time, file_ts):
-
-    try:
-        dt1 = parse(obj_time, ignoretz=True)
-        dt2 = datetime.utcfromtimestamp(file_ts)
-        rd = relativedelta(dt2, dt1)
-    except:
-        return 'Unknown'
-
-    if rd.days > 0:
-        return str(rd.days)+'d'
-    elif rd.hours > 9:
-        return str(rd.hours)+'h'
-    elif rd.hours > 0 and rd.hours < 10:
-        return str(rd.hours)+'h'+str(rd.minutes)+'m'
-    elif rd.minutes > 9:
-        return str(rd.minutes) + 'm'
-    elif rd.minutes > 0 and rd.minutes < 10:
-        return str(rd.minutes)+'m'+str(rd.seconds)+'s'
-    else:
-        return str(rd.seconds)+'s'
-
-
-def age2(obj_time, obj2_time):
+def age(ts1, ts2, ts1_type='iso', ts2_type='epoch'):
 
     try:
-        dt1 = parse(obj_time, ignoretz=True)
-        dt2 = parse(obj2_time, ignoretz=True)
+        if ts1_type == 'iso':
+            dt1 = parse(ts1, ignoretz=True)
+        elif ts1_type == 'epoch':
+            dt1 = datetime.utcfromtimestamp(ts1)
+    
+        if ts2_type == 'iso':
+            dt2 = parse(ts2, ignoretz=True)
+        elif ts2_type == 'epoch':
+            dt2 = datetime.utcfromtimestamp(ts2)        
+        
         rd = relativedelta(dt2, dt1)
     except:
         return 'Unknown'
