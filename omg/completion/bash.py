406eed0bfaf60315079b85ef6d70d1dbef865de6
Robert Bost <rbost@redhat.com>
2021-02-17 13:56:49 -0700

    Always use -o default for bash completion. Resolves #40.

--- omg/completion/bash.py
+++ omg/completion/bash.py
@@ -10,11 +10,11 @@ _omg_completion() {
 }
 
 _omg_completionetup() {
-    local COMPLETION_OPTIONS=""
+    local COMPLETION_OPTIONS="-o default"
     local BASH_VERSION_ARR=(${BASH_VERSION//./ })
     # Only BASH version 4.4 and later have the nosort option.
     if [ ${BASH_VERSION_ARR[0]} -gt 4 ] || ([ ${BASH_VERSION_ARR[0]} -eq 4 ] && [ ${BASH_VERSION_ARR[1]} -ge 4 ]); then
-        COMPLETION_OPTIONS="-o nosort -o default"
+        COMPLETION_OPTIONS="-o nosort"
     fi
     
     complete $COMPLETION_OPTIONS -F _omg_completion omg
