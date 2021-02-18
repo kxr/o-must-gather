71b5424efc49b50b928f09102c957271cc9c2153
Khizer <khizernaeem@gmail.com>
2021-02-18 09:44:06 +0400

    Adding -o default when bash is 4.4

--- omg/completion/bash.py
+++ omg/completion/bash.py
@@ -14,7 +14,7 @@ _omg_completionetup() {
     local BASH_VERSION_ARR=(${BASH_VERSION//./ })
     # Only BASH version 4.4 and later have the nosort option.
     if [ ${BASH_VERSION_ARR[0]} -gt 4 ] || ([ ${BASH_VERSION_ARR[0]} -eq 4 ] && [ ${BASH_VERSION_ARR[1]} -ge 4 ]); then
-        COMPLETION_OPTIONS="-o nosort"
+        COMPLETION_OPTIONS="-o nosort -o default"
     fi
     
     complete $COMPLETION_OPTIONS -F _omg_completion omg
