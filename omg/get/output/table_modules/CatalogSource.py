895aaac09378b92c17e4f7b195b90c05e7edcd2c
Khizer Naeem <khizernaeem@gmail.com>
2022-05-17 04:04:08 +0400

    table output module for CatalogSource

---
+++ omg/get/output/table_modules/CatalogSource.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from omg.utils.dget import dget
+
+
+def _col_display(res):
+    return dget(res, ["res", "spec", "displayName"])
+
+
+def _col_type(res):
+    return dget(res, ["res", "spec", "sourceType"])
+
+
+def _col_publisher(res):
+    return dget(res, ["res", "spec", "publisher"])
+
+
+# Default columns (without -o wide)
+# NAME and AGE cols, if present, with None value,
+# will be handled by build_table function that will
+# fill them with the common name/age column functions
+DEFAULT_COLUMNS = {
+    "NAME": None,
+    "DISPLAY": _col_display,
+    "TYPE": _col_type,
+    "PUBLISHER": _col_publisher,
+    "AGE": None
+}
+
+# Wide columns (with -o wide)
+# In addition to the default columns
+WIDE_COLUMNS = {
+}
